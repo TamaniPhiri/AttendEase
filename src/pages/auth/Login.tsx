87035ae482a0d7fbbd70d702aa7a4136f4224ad0
tamani phiri <tamanigabriel0@gmail.com>
2024-03-19 13:08:52 +0200

    c

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -2,24 +2,30 @@ const Login = () => {
   return (
     <section className="min-h-screen w-full px-4 flex flex-col md:px-12 items-center justify-center">
       <h1 className=" font-bold text-2xl">
-        Attend <span className=" text-purple-500">Ease</span>
+        Attend<span className=" text-purple-500">Ease</span>
       </h1>
-      <div className="lg:max-w-md flex flex-col w-full gap-4">
+      <div className="lg:max-w-sm flex flex-col w-full gap-4 my-6">
         <input
           type="text"
           placeholder="Register No."
-          className=" border focus:outline-none"
+          className=" border focus:outline-none md:p-4 p-3 focus:outline-purple-400"
         />
         <input
           type="email"
           placeholder="Email"
-          className=" border focus:outline-none"
+          className=" border focus:outline-none md:p-4 p-3 focus:outline-purple-400"
         />
         <input
           type="password"
           placeholder="Password"
-          className=" border focus:outline-none"
+          className=" border focus:outline-none md:p-4 p-3 focus:outline-purple-400"
         />
+        <a
+          href="/"
+          className=" md:p-4 p-3 hover:bg-purple-500 text-center font-semibold bg-purple-600 text-white"
+        >
+          Login
+        </a>
       </div>
     </section>
   );
