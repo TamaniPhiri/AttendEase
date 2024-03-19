e80ced9d1b60683f444ecdc92c33ddd8163424cf
tamani phiri <tamanigabriel0@gmail.com>
2024-03-19 13:24:38 +0200

    c

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
           Attend<span className=" text-purple-500">Ease</span>
         </h1>
       </div>
-      <div className=" flex items-center gap-8">
+      <div className=" flex items-center gap-8 text-white">
         <Link to={"/home"}>link 1</Link>
         <Link to={"/home"}>link 2</Link>
         <Link to={"/home"}>link 3</Link>
