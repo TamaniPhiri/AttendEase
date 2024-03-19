4ae2ee753908a8d3a696b0c7b12c1070ed908911
tamani phiri <tamanigabriel0@gmail.com>
2024-03-19 13:05:16 +0200

    c

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,9 +1,26 @@
 const Login = () => {
   return (
-    <section className="min-h-screen w-full px-4 md:px-12 items-center justify-center">
-      <h1>
-        Attend <span>Ease</span>
+    <section className="min-h-screen w-full px-4 flex flex-col md:px-12 items-center justify-center">
+      <h1 className=" font-bold text-2xl">
+        Attend <span className=" text-purple-500">Ease</span>
       </h1>
+      <div className="lg:max-w-md flex flex-col w-full gap-4">
+        <input
+          type="text"
+          placeholder="Register No."
+          className=" border focus:outline-none"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          className=" border focus:outline-none"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className=" border focus:outline-none"
+        />
+      </div>
     </section>
   );
 };
