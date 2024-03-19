35867098cf6e05125fdb261375a02ccbe1f681d2
tamani phiri <tamanigabriel0@gmail.com>
2024-03-19 13:20:35 +0200

    c

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/auth/Login";
 import Home from "./pages/Home";
+import Register from "./pages/auth/Register";
 
 function App() {
   return (
     <Routes>
       <Route path="/" Component={Login} />
+      <Route path="/register" Component={Register} />
       <Route path="/home" Component={Home} />
     </Routes>
   );
